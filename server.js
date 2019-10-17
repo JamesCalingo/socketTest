4e9bc7a258d980c4597c8c5cb7a308cf5cb406d8
JC <james.calingo@gmail.com>
2019-10-17 13:15:13 -0400

    working on username integration

--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const http = require('http').createServer(app);
 const io = require("socket.io")(http)
 const port = process.env.PORT || 3000
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log("someone's here!")
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  console.log("Someone new is here!");
+
+  socket.username = "Anonymous";
+
+  socket.on("change username", function (data) {
+    socket.username = data.username
+  })
+
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
+  socket.on("disconnect", function () {
+    console.log("Someone left the room!")
+  })
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on port:' + port);
 });
